ec2cff8f7947bf2bf45c468bd6a3944e8287b620
wizo <westwizo@yahoo.com>
2025-08-30 00:20:39 +0100

    build fixed

--- src/integrationTest/java/uk/gov/hmcts/reform/dev/controllers/GetWelcomeTest.java
+++ src/integrationTest/java/uk/gov/hmcts/reform/dev/controllers/GetWelcomeTest.java
@@ -4,18 +4,24 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
+import uk.gov.hmcts.reform.dev.services.TaskService;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest
+@WebMvcTest(controllers = RootController.class) // Specify only RootController
 class GetWelcomeTest {
 
     @Autowired
     private transient MockMvc mockMvc;
+
+    // Mock the TaskService since TaskController is not under test here
+    @MockBean
+    private TaskService taskService;
 
     @DisplayName("Should welcome upon root request with 200 response code")
     @Test
